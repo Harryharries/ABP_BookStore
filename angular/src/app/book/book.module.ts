220e87c9551ca052325dab8b00ebfb66c74222e2
HarryGu <harry.guapi@gmail.com>
2023-08-03 09:14:01 -0600

    created basic function

--- angular/src/app/book/book.module.ts
+++ angular/src/app/book/book.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { BookRoutingModule } from './book-routing.module';
 import { BookComponent } from './book.component';
-
+import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    BookComponent
-  ],
+  declarations: [BookComponent],
   imports: [
-    CommonModule,
-    BookRoutingModule
+    BookRoutingModule,
+    NgbDatepickerModule,
+    SharedModule
   ]
 })
 export class BookModule { }
